a51acc8e9fb1d75391f0760af3e6bf120a00050e
MoyArt Maximus <moyart@gmail.com>
2018-08-15 23:09:51 -0500

    Hello world component created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 /* Create a new component.*/
 
+const App = function(){
+    return <div>Hello!!</div>;
+}
+
 /* Add component to the DOM, Take this components generated HTML and put it on the Page*/
