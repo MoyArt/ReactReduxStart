bca382b0326fe0d3a39762d8bb5d37317002359c
MoyArt Maximus <moyart@gmail.com>
2018-08-19 23:23:58 -0500

    Added Searchbar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    render(){
+        return <input type="text" id="SearchBar" placeholder="Search..." />
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyDGdCpzKCvEQqciOEkEjOhRj_hMxyhb9rw';
 /* Create a new component.*/
 
 const App = function(){
-    return <div>Hello!!</div>;
+    return (
+        <div class="search-bar">
+            <SearchBar/>
+        </div>
+    );
 }
+
 
 /* Add component to the DOM, Take this components generated HTML and put it on the Page*/
 ReactDOM.render(<App />, document.querySelector(".container"));
