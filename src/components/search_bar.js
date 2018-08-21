62ef34390678cf21ebfbc4cb699af2206fecaf1f
MoyArt Maximus <moyart@gmail.com>
2018-08-20 23:19:18 -0500

    Added onInputChange method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,6 +4,9 @@ class SearchBar extends React.Component{
     render(){
         return <input type="text" id="SearchBar" placeholder="Search..." />
     }
+    onInputChange(e){
+
+    }
 }
 
 export default SearchBar;
