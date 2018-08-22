e4a644e6bac731511d9cb599c1702ba9a0c3e4e1
MoyArt Maximus <moyart@gmail.com>
2018-08-21 23:52:03 -0500

    added constructor method and state to SearchBar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {term: ""};
+    }
     render(){
         return <input type="text" id="SearchBar" placeholder="Search..." onChange={this.onInputChange}/>
     }
     onInputChange(e){
-
+        console.log(e.target.value);
     }
 }
 
