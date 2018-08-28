022f5fd6f0ae6a1e08dac43fb63d6db19c6f3ed8
MoyArt Maximus <moyart@gmail.com>
2018-08-27 22:22:26 -0500

    Search-bar component change from functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDGdCpzKCvEQqciOEkEjOhRj_hMxyhb9rw';
-YTSearch({key:API_KEY, term: '3D printers'}, function(data){
-    console.log(data)
-})
+
 /* Create a new component.*/
 
-const App = function(){
-    return (
-        <div class="search-bar">
-            <SearchBar/>
-        </div>
-    );
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key:API_KEY, term: '3D printers'}, (data) => {
+            console.log(data);
+            this.setState({videos:data});
+        });
+    }
+    render(){
+        return (
+            <div class="search-bar">
+                <SearchBar/>
+            </div>
+        );
+    }
 }
 
 
