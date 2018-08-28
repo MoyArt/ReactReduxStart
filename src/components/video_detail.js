50f0e33b00828ab2d6e992c24f472a38f6f3f5f3
MoyArt Maximus <moyart@gmail.com>
2018-08-27 23:57:06 -0500

    parsed video detal info to video detail component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const VideoDetail = (props) =>{
+    const video = props.video;
+    const videoTitle = video.snippet.title;
+    const videoDescription = video.snippet.description; 
+    const videoID = video.id.videoId;
+    const videoURL = `https://www.youtube.com/embed/${videoID}`;
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item"></iframe>
+                <iframe className="embed-responsive-item" src={videoURL}></iframe>
             </div>
             <div className="details">
-                <div></div>
-                <div></div>
+                <div>{videoTitle}</div>
+                <div>{videoDescription}</div>
             </div>
         </div>
     )
